bc0fc8c999ae805007c84d26a2d2a1f872b122e0
hungoke <nvh66725@gmail.com>
2021-03-07 13:48:41 +0700

    commit context

--- src/components/todoApp/Tasks.js
+++ src/components/todoApp/Tasks.js
@@ -4,28 +4,29 @@ import './../../App.css'
 import Task from './Task'
 
 class Tasks extends React.Component {
-  get filterByTag() {
-    const { tasks, currentTag, tags } = this.context
-    let tagName = ''
+  // get filterByTag() {
+  //   const { tasks, currentTag, tags } = this.context
+  //   let tagName = ''
 
-    if (currentTag === 0) {
-      return tasks
-    } else {
-      tags.forEach(tag => {
-        if (tag.id === currentTag) {
-          tagName = tag.name
-        }
-      })
-    }
+  //   if (currentTag === 0) {
+  //     return tasks
+  //   } else {
+  //     tags.forEach(tag => {
+  //       if (tag.id === currentTag) {
+  //         tagName = tag.name
+  //       }
+  //     })
+  //   }
 
-    return tasks.filter(task => task.tag === tagName)
-  }
+  //   return tasks.filter(task => task.tag === tagName)
+  // }
 
   render() {
+    const { tasks } = this.context
     return (
       <>
         {
-          this.filterByTag.map(task => (
+          tasks.map(task => (
             <Task
               key={task.id}
               task={task}
